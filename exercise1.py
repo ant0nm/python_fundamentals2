d1dc74e3d0ea293bab4c3f71fbd91acc98f46625
Anton <anton.mois33v@gmail.com>
2019-02-12 14:57:58 -0500

    Completed exercise 1

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+# double up
+def double(my_number):
+    return my_number * 2
+
+# test out the function, display the results
+print(double(50))
+print(double(-50))
+print(double(0))
+print(double(0.5))
